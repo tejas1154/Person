3862460c3a874aa30a1493ce57139c131291da0a
tejas1154 <tejas.ramane@aurionpro.com>
2025-06-29 18:37:30 +0530

    Person part upload

---
+++ Person.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    internal class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age {  get; set; }
+        public double Height { get; set; }
+        public double Weight { get; set; }
+
+        public Person(int id, string name, int age, double height, double weight)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+            Height = height;
+            Weight = weight;
+        }
+
+        public Person(int id, string name, int age)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+            Height = 5.0;
+            Weight = 50.0;
+        }
+
+        public double CalculateBMI()
+        {
+            return Weight / (Height * Height);
+        }
+
+        public string GetBodyType()
+        {
+            double bmi = CalculateBMI();
+            if (bmi > 18.5)
+                return "Underweight";
+            else if (bmi >= 18.5 && bmi < 24.9)
+                return "Normal Weight";
+            else if (bmi >= 25 && bmi < 29.9)
+                return "Overweight";
+            else
+                return "Obese";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Person
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person(1, "ABC", 25, 1.65, 60);
+            Console.WriteLine($"{person1.Name}'s BMI: {person1.CalculateBMI()}");
+            Console.WriteLine($"{person1.Name}'s Body Type: {person1.GetBodyType()}");
+
+            Person person2 = new Person(2, "XYZ", 39);
+            Console.WriteLine($"{person2.Name}'s BMI: {person2.CalculateBMI()}");
+            Console.WriteLine($"{person2.Name}'s Body Type: {person2.GetBodyType()}");
+        }
+    }
+}
